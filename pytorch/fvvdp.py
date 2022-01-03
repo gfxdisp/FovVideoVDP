0fc488ef63a91a6b562b951222fabe12010fb652
Rafal Mantiuk <mantiuk@gmail.com>
2022-01-03 12:12:56 +0000

    Work in progress on the new python interface (porting to python)

--- pytorch/fvvdp.py
+++ pytorch/fvvdp.py
@@ -1,5 +1,7 @@
+from abc import abstractmethod
 from numpy.lib.shape_base import expand_dims
 import torch
+from torch.functional import Tensor
 import torch.nn.functional as Func
 import numpy as np 
 import os
@@ -22,6 +24,8 @@ from hdrvdp_lpyr_dec import hdrvdp_lpyr_dec
 from interp import interp1, interp3
 from utils import *
 from fvvdp_test import FovVideoVDP_Testbench
+
+from fvvdp_display_model import fvvdp_display_photometry, fvvdp_display_geometry
 
 class DisplayModel():
     def __init__(self, W, H, diagonal_fov_degrees, distance_m, diag_size_m, min_luminance, max_luminance, gamma_func, rgb2X, rgb2Y, rgb2Z):
@@ -931,3 +935,165 @@ if __name__ == '__main__':
 
             print("Torch Benchmark:")
             print(bench.timeit(100))
+
+
+class fvvdp_video_source:
+   
+    # Return (height, width, frames) touple with the resolution and
+    # the length of the video clip.
+    @abstractmethod
+    def get_video_size(self):
+        pass
+
+    # Return the frame rate of the video
+    @abstractmethod
+    def get_frames_per_second(self) -> int:
+        pass
+    
+    # Get a pair of test and reference video frames as a single-precision luminance map
+    # scaled in absolute inits of cd/m^2. 'frame' is the frame index,
+    # starting from 1. 
+    @abstractmethod
+    def get_test_frame( self, frame, device ) -> Tensor:
+        pass
+
+    @abstractmethod
+    def get_reference_frame( self, frame, device ) -> Tensor:
+        pass
+
+
+# A video source that takes loaded video frames as (height x width x 3 x
+# frames) matrices. This class is for display-encoded (gamma-encoded)
+# content that must be processed by a display model to produce linear
+# absolute luminance emitted from a display.
+class fvvdp_video_source_dm( fvvdp_video_source ):
+               
+    # test_video, reference video - tensor of the size:
+    #   [height width 3] - for colour images
+    #   [height width 3 N] - for colour video of N frames
+    #   [height width N] - for grayscale video of N frames
+    # fps - frames per second. Must be 0 for images
+    # display_model - object that implements fvvdp_display_photometry
+    #   class
+    # color_space_name - name of the colour space (see
+    #   fvvdp_data/color_spaces.json)
+    def __init__( self, test_video, reference_video, fps, display_model, color_space_name ):
+        
+        if test_video.shape != reference_video.shape:
+            raise RuntimeError( 'Test and reference image/video tensors must be exactly the same size' )
+        
+        if fps==0 and ((test_video.dim==3 and test_video.shape[0]>3) or (test_video.dim==4 and test_video.shape[1]>1)):
+            raise RuntimeError( 'When passing video sequences, you must set ''frames_per_second'' parameter' )
+        
+        self.fps = fps
+        self.is_video = (fps>0)
+        #TODO: self.is_color = (~vs.is_video and size(test_video,3)==3) or (vs.is_video and length(size(test_video))==4 and size(test_video,3)==3);
+        self.test_video = test_video
+        self.reference_video = reference_video
+        
+    #     col_spaces = vs.load_json( 'color_spaces.json' );
+        
+    #     if ~exist( 'color_space', 'var' ) || isempty( color_space_name )
+    #         color_space_name = 'sRGB';
+    #     end
+        
+    #     if ~isfield( col_spaces, color_space_name )
+    #         error( 'Unknown color space "%s". Check display_models/color_spaces.json for available color spaces.', color_space_name );
+    #     end
+    #     cs = col_spaces.(color_space_name);
+    #     vs.color_to_luminance = cs.RGB2Y;                                                           
+        
+    #     if ~exist( 'display_model', 'var' ) || isempty( display_model )
+    #         display_model = 'sdr_4k_30';
+    #     end
+        
+    #     if ischar( display_model )
+    #         disp_models = vs.load_json( 'display_models.json' );
+
+    #         if ~isfield( disp_models, display_model )
+    #             error( 'Unknown display model "%s". Check display_models/display_models.json for available display models.', display_model );
+    #         end
+    #         dm = disp_models.(display_model);
+                        
+    #         vs.display_model = fvvdp_display_photometry.create_from_json( dm );
+    #     else
+    #         vs.display_model = display_model;
+    #     end
+        
+    # end
+
+    # function fps = get_frames_per_second(vs)
+    #     fps = vs.fps;
+    # end
+    
+    # % Return a [height width frames] vector with the resolution and
+    # % the number of frames in the video clip. [height width 1] is
+    # % returned for an image. 
+    # function sz = get_video_size(vs)
+    #     if vs.is_color
+    #         sz = [size(vs.reference_video,1) size(vs.reference_video,2) size(vs.reference_video,4)];
+    #     else
+    #         sz = size(vs.reference_video);
+    #         if length(sz)==2
+    #             sz = [ sz 1 ];
+    #         end
+    #     end
+    # end
+    
+    # % Get a test video frame as a single-precision luminance map
+    # % scaled in absolute inits of cd/m^2. 'frame' is the frame index,
+    # % starting from 1. If use_gpu==true, the function should return a
+    # % gpuArray.
+    # function L_test = get_test_frame( vs, frame, use_gpu )
+    #     L_test = vs.get_frame_(vs.test_video, frame, use_gpu);            
+    # end
+    
+    # function L_ref = get_reference_frame( vs, frame, use_gpu )
+    #     L_ref = vs.get_frame_(vs.reference_video, frame, use_gpu);            
+    # end
+    
+    # function L = get_frame_( vs, from_video, frame, use_gpu )
+        
+    #     % Determine the maximum value of the data type storing the
+    #     % image/video
+    #     if isa( from_video, 'uint8' ) || isa( from_video, 'uint16' )
+    #         peak_value = single(intmax( class(from_video) ));
+    #     elseif isa( from_video, 'logical' ) || isa( from_video, 'single' ) || isa( from_video, 'double' )
+    #         peak_value = 1.0;
+    #     else
+    #         error( 'unknown data type of the video/image' );
+    #     end
+        
+    #     if vs.is_color
+    #         L = single(from_video(:,:,:,frame));
+    #     else
+    #         L = single(from_video(:,:,frame));
+    #     end
+        
+    #     if use_gpu
+    #         L = gpuArray(L);
+    #     end
+    #     L = vs.display_model.forward( L/peak_value );
+        
+    #     if vs.is_color
+    #         % Convert to grayscale
+    #         L = L(:,:,1)*vs.color_to_luminance(1) + L(:,:,2)*vs.color_to_luminance(2) + L(:,:,3)*vs.color_to_luminance(3);
+    #     end
+        
+
+
+class fvvdp:
+    def __init__(self, display_name="standard_4k", display_photometry=None, display_geometry=None, color_space="rec2020", foveated=False, heatmap=None, quiet=False):
+        self.quiet = quiet
+        self.foveated = foveated
+        self.heatmap = heatmap
+
+        if display_photometry is None:
+            display_photometry = fvvdp_display_photometry.load(display_name)
+
+        if display_geometry is None:
+            display_geometry = fvvdp_display_geometry.load(display_name)
+
+    def predict(self, test_cont, reference_cont):
+        pass
+        
--- pytorch/fvvdp_display_model.py
+++ pytorch/fvvdp_display_model.py
@@ -1,8 +1,12 @@
+from abc import ABC, abstractmethod
 import torch
 import torch.nn.functional as Func
 import numpy as np 
 import math
 import logging
+import os
+
+from utils import json2dict
 
 # Convert pixel values to linear RGB using sRGB non-linearity
 # 
@@ -14,8 +18,68 @@ def srgb2lin( p ):
     L = torch.where(p > 0.04045, ((p + 0.055) / 1.055)**2.4, p/12.92)
     return L
 
+class fvvdp_display_photometry:
 
-class fvvdp_display_photo_gog: 
+    # Transforms gamma-encoded pixel values V, which must be in the range
+    # 0-into absolute linear colorimetric values emitted from
+    # the display.
+    @abstractmethod
+    def forward( self, V ):
+        pass
+
+    # Print the display specification    
+    @abstractmethod
+    def print( self ):
+        pass
+
+    @classmethod
+    def load( cls, display_name ):
+        models_file      = os.path.join(os.path.dirname(__file__), "../fvvdp_data/display_models.json")
+
+        models = json2dict(models_file)
+
+        for mk in models:
+            if mk == display_name:
+                model = models[mk]
+
+                Y_peak = model["max_luminance"]
+
+                if "min_luminance" in model:
+                    contrast = Y_peak/model["min_luminance"]
+                elif "contrast" in model:
+                    contrast = model["contrast"]
+                else:
+                    contrast = 500
+
+                # Ambient light
+                if "E_ambient" in model:
+                    E_ambient = model["E_ambient"]
+                else:
+                    E_ambient = 0
+                
+                # Reflectivity of the display panel
+                if "k_refl" in model: 
+                    k_refl = model["k_refl"]
+                else:
+                    k_refl = 0.005
+
+                # Reflectivity of the display panel
+                if "gamma" in model: 
+                    gamma = model["gamma"]
+                else:
+                    gamma = 2.2
+  
+                obj = fvvdp_display_photo_gog( Y_peak, contrast, gamma, E_ambient, k_refl)
+                obj.full_name = model["name"]
+                obj.short_name = display_name
+
+                return obj
+
+        raise RuntimeError( 'Unknown display specification "{disp_name}"'.format(disp_name=display_name))
+
+
+
+class fvvdp_display_photo_gog(fvvdp_display_photometry): 
     # Gain-gamma-offset display model to simulate SDR displays
     # Object variables:
     #  Y_peak - display peak luminance in cd/m^2
@@ -47,7 +111,7 @@ class fvvdp_display_photo_gog:
     # https://www.cl.cam.ac.uk/~rkm38/pdfs/mantiuk2016perceptual_display.pdf
     #
     # Copyright (c) 2010-2021, Rafal Mantiuk
-    def __init__( self, Y_peak, contrast = 1000, gamma = -1, E_ambient = 0, k_refl = 0.005 ):
+    def __init__( self, Y_peak, contrast = 1000, gamma = 2.2, E_ambient = 0, k_refl = 0.005 ):
             
         self.Y_peak = Y_peak            
         self.contrast = contrast
@@ -56,8 +120,8 @@ class fvvdp_display_photo_gog:
         self.k_refl = k_refl
     
         
-    # Transforms gamma-correctec pixel values V, which must be in the range
-    # 0-1, into absolute linear colorimetric values emitted from
+    # Transforms gamma-encoded pixel values V, which must be in the range
+    # 0-into absolute linear colorimetric values emitted from
     # the display.
     def forward( self, V ):
         
@@ -79,12 +143,14 @@ class fvvdp_display_photo_gog:
         return self.Y_peak
 
 
+    # Get the effective black level, accounting for screen reflections
     def get_black_level( self ):
         Y_refl = self.E_ambient/math.pi*self.k_refl  # Reflected ambient light            
         Y_black = Y_refl + self.Y_peak/self.contrast
 
         return Y_black
-    
+
+    # Print the display specification    
     def print( self ):
         Y_black = self.get_black_level()
         
@@ -95,4 +161,165 @@ class fvvdp_display_photo_gog:
         logging.info( '  Ambient light: {} lux'.format( self.E_ambient ) )
         logging.info( '  Display reflectivity: {}%'.format( self.k_refl*100 ) )
     
+
+# Use this class to compute the effective resolution of a display in pixels
+# per degree (ppd). The class accounts for the change in the projection
+# when looking at large FOV displays (e.g. VR headsets) at certain
+# eccentricity.
+#
+# The class is also useful for computing the size of a display in meters
+# and visual degrees. Check 'display_size_m' and 'display_size_deg' class
+# properties for that.
+#
+# R = fvvdp_display_geometry(resolution, distance_m=None, distance_display_heights=None, 
+#                            fov_horizontal=None, fov_vertical=None, fov_diagonal=None, 
+#                            diagonal_size_inches=None)
+#
+# resolution is the 2-element touple with the pixel resolution of the
+# display: (horizontal_resolutution, vertical_resolutution)
+# distance_m - viewing distance in meters
+# distance_display_heights - viewing distance in the heights of a display
+# fov_horizontal - horizontal field of view of the display in degrees
+# fov_vertical - vertical field of view of the display in degrees
+# fov_diagonal - diagonal field of view of the display in degrees
+# diagonal_size_inches - display diagonal resolution in inches
+#
+# Examples:
+# # HTC Pro
+# # Note that the viewing distance must be specified even though the resolution
+# # and 'fov_diagonal' are enough to find pix_per_deg.
+# R = fvvdp_display_geometry( (1440, 1600), distance_m=3, fov_diagonal=110 )
+# R.get_ppd( torch.tensor( [0, 10, 20, 30, 40])) # pix per deg at the given eccentricities
+#
+# # 30" 4K monitor seen from 0.6 meters
+# R = fvvdp_display_geometry( (3840, 2160), diagonal_size_inches=30, distance_m=0.6 )
+# R.get_ppd()
+#
+# # 47" SIM2 display seen from 3 display heights
+# R = fvvdp_display_geometry( (1920, 1080), diagonal_size_inches=47, distance_display_heights=3 )
+# R.get_ppd()
+#
+# Some information about the effective FOV of VR headsets
+# http://www.sitesinvr.com/viewer/htcvive/index.html
+class fvvdp_display_geometry:
+
+    def __init__(self, resolution, distance_m=None, distance_display_heights=None, fov_horizontal=None, fov_vertical=None, fov_diagonal=None, diagonal_size_inches=None) -> None:
+
+        self.resolution = resolution
+        
+        ar = resolution[0]/resolution[1] # width/height
+        
+        if not diagonal_size_inches is None:
+            height_mm = math.sqrt( (diagonal_size_inches*25.4)**2 / (1+ar**2) )
+            self.display_size_m = (ar*height_mm/1000, height_mm/1000)
+                
+        if (not distance_m is None) and (not distance_display_heights is None):
+            raise RuntimeError( 'You can pass only one of: ''distance_m'', ''distance_display_heights''.' )
+        
+        if not distance_m is None:
+            self.distance_m = distance_m;
+        elif not distance_display_heights is None:
+            if not hasattr( self, "display_size_m" ):
+                raise RuntimeError( 'You need to specify display diagonal size ''diagonal_size_inches'' to specify viewing distance as ''distance_display_heights'' ' )
+            self.distance_m = distance_display_heights * self.display_size_m[1]
+        elif (not fov_horizontal is None) or (not fov_vertical is None) or (not fov_diagonal is None):
+            # Default viewing distance for VR headsets
+            self.distance_m = 3
+        else:
+            raise RuntimeError( 'Viewing distance must be specified as ''distance_m'' or ''distance_display_heights''.' )
+        
+        if ((not fov_horizontal is None) + (not fov_vertical is None) + (not fov_diagonal is None)) > 1:
+            raise RuntimeError( 'You can pass only one of ''fov_horizontal'', ''fov_vertical'', ''fov_diagonal''. The other dimensions are inferred from the resolution assuming that the pixels are square.' )
+        
+        if not fov_horizontal is None:
+            width_m = 2*math.tan( math.radians(fov_horizontal/2) )*self.distance_m
+            self.display_size_m = (width_m, width_m/ar)
+        elif not fov_vertical is None:
+            height_m = 2*math.tan( math.radians(fov_vertical/2) )*self.distance_m
+            self.display_size_m = (height_m*ar, height_m)
+        elif not fov_diagonal is None:
+            # Note that we cannot use Pythagorean theorem on degs -
+            # we must operate on a distance measure
+            # This is incorrect: height_deg = p.Results.fov_diagonal / sqrt( 1+ar^2 );
+            
+            distance_px = math.sqrt(self.resolution[0]**2 + self.resolution[1]**2) / (2.0 * math.tan( math.radians(fov_diagonal*0.5)) )
+            height_deg = math.degrees(math.atan( self.resolution[1]/2 / distance_px ))*2
+            
+            height_m = 2*math.tan( math.radians(height_deg/2) )*self.distance_m
+            self.display_size_m = (height_m*ar, height_m)
+        
+        self.display_size_deg = ( 2 * math.degrees(math.atan( self.display_size_m[0] / (2*self.distance_m) )), \
+                                  2 * math.degrees(math.atan( self.display_size_m[1] / (2*self.distance_m) )) )
+        
+    # Get the number of pixels per degree
+    #
+    # ppd = R.get_ppd()
+    # ppd = R.get_ppd(eccentricity)
+    #
+    # eccentricity is the viewing angle from the center in degrees. If
+    # not specified, the central ppd value (for 0 eccentricity) is
+    # returned.
+    def get_ppd(self, eccentricity = None):
+        
+        # if ~isempty( dr.fixed_ppd )
+        #     ppd = dr.fixed_ppd;
+        #     return;
+        # end
+        
+        # pixel size in the centre of the display
+        pix_deg = 2*math.degrees(math.atan( 0.5*self.display_size_m[0]/self.resolution[0]/self.distance_m ))
+        
+        base_ppd = 1/pix_deg
+        
+        if eccentricity is None:
+            return base_ppd
+        else:
+            delta = pix_deg/2
+            tan_delta = math.tan(math.radians(delta))
+            tan_a = torch.tan( torch.deg2rad(eccentricity) )
+            
+            ppd = base_ppd * (torch.tan(torch.deg2rad(eccentricity+delta))-tan_a)/tan_delta
+            return ppd
+
+    def print(self):
+        logging.info( 'Geometric display model:' )
+        if hasattr( self, "fixed_ppd" ):
+            logging.info( '  Fixed pixels-per-degree: {}'.format(self.fixed_ppd) )
+        else:
+            logging.info( '  Resolution: {w} x {h} pixels'.format( w=self.resolution[0], h=self.resolution[1] ) )
+            logging.info( '  Display size: {w:.1f} x {h:.1f} cm'.format( w=self.display_size_m[0]*100, h=self.display_size_m[1]*100) )
+            logging.info( '  Display size: {w:.2f} x {h:.2f} deg'.format( w=self.display_size_deg[0], h=self.display_size_deg[1] ) )
+            logging.info( '  Viewing distance: {d:.3f} m'.format(d=self.distance_m) )
+            logging.info( '  Pixels-per-degree (center): {ppd:.2f}'.format(ppd=self.get_ppd()) )
+
+    @classmethod
+    def load( cls, display_name ):
+        models_file      = os.path.join(os.path.dirname(__file__), "../fvvdp_data/display_models.json")
+        models = json2dict(models_file)
+
+        for mk in models:
+            if mk == display_name:
+                model = models[mk]
+                assert "resolution" in model
+
+                inches_to_meters = 0.0254
+
+                W, H = model["resolution"]
+
+                if "fov_diagonal" in model: fov_diagonal = model["fov_diagonal"]
+                else:                       fov_diagonal = None
+
+                if   "viewing_distance_meters" in model: distance_m = model["viewing_distance_meters"]
+                elif "viewing_distance_inches" in model: distance_m = model["viewing_distance_inches"] * inches_to_meters
+                else:                                    distance_m = None
+
+                if   "diagonal_size_meters" in model: diag_size_inch = model["diagonal_size_meters"] / inches_to_meters
+                elif "diagonal_size_inches" in model: diag_size_inch = model["diagonal_size_inches"] 
+                else:                                 diag_size_inch = None
+
+                obj = fvvdp_display_geometry( (W, H), distance_m=distance_m, fov_diagonal=fov_diagonal, diagonal_size_inches=diag_size_inch)
+                return obj
+
+        logging.error("Error: Display model '%s' not found in display_models.json" % display_name)
+        return None
 
