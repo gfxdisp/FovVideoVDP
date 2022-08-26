e952be4abdb334ec59f291d1e16d97faec427049
param <param.catchchaos@gmail.com>
2022-08-26 22:07:52 +0100

    Removed some setup files by incorporating into setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyfvvdp',
     version='1.0.0',
     description='Pytorch code accompanying "FovVideoVDP": a full-reference' \
                 'visual quality metric that predicts the perceptual' \
                 'difference between pairs of images and videos.',
+    long_description=long_description,
     url='https://github.com/gfxdisp/FovVideoVDP',
+    long_description_content_type='text/markdown',
     author='Rafał K. Mantiuk',
     author_email='mantiuk@gmail.com',
     license='Creative Commons',
     packages=['pyfvvdp', 'pyfvvdp/third_party'],
+    package_data={'pyfvvdp': ['csf_cache/*.mat', 'fvvdp_data/*.json']},
     include_package_data=True,
     install_requires=['numpy==1.23.1',
                       'scipy==1.8.1',
@@ -22,7 +28,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'Operating System :: POSIX :: Linux',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
