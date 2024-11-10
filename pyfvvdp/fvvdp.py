ec7c220d21eebd9b16a7f346ddf78df3d54acf94
Rafal Mantiuk <mantiuk@gmail.com>
2024-11-10 11:28:17 +0000

    More robust handling of expands_dims

--- pyfvvdp/fvvdp.py
+++ pyfvvdp/fvvdp.py
@@ -1,6 +1,9 @@
 from abc import abstractmethod
 from urllib.parse import ParseResultBytes
-from numpy import expand_dims
+try:
+    from numpy import expand_dims
+except ImportError:
+    from numpy.lib.shape_base import expand_dims
 import torch
 from torch.utils import checkpoint
 from torch.functional import Tensor
